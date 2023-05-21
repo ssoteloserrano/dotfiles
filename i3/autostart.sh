f0699fc16f3d50eef28edec56241c4a1b10c19f9
scarktt <ssoteloserrano@gmail.com>
2023-05-21 15:49:21 -0600

    feat(i3): first commit

---
+++ i3/autostart.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# monitors
+xrandr --output DP-1 --primary --auto --rotate normal --output DP-3 --auto --rotate right &
+
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# notification daemon
+dunst -config ~/.config/dunst/dunstrc &
+
+# compositor
+while pgrep -u $UID -x picom > /dev/null; do sleep 1; done
+picom -b &
+
+# status bar
+~/.config/polybar/launch.sh &
+
+# feh --bg-fill /home/$USER/Pictures/Wallpapers/1.jpg &
+
+# auto sleep mode
+xset s off -dpms &
+
+# keyboard layout
+setxkbmap us altgr-intl &
