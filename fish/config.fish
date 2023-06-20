e61631d956c39320bb6cea3f1ba5d546f15659a9
scarktt <ssoteloserrano@gmail.com>
2023-06-19 21:25:43 -0600

    style: add new custom theme

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# oh-my-posh init fish --config ~/half-life.omp.json | source
-oh-my-posh init fish --config ~/zash.omp.json | source
-
-
+oh-my-posh init fish --config ~/.config/.poshthemes/zash.modified.json | source
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/scarlett/google-cloud-sdk/path.fish.inc' ]; . '/home/scarlett/google-cloud-sdk/path.fish.inc'; end
