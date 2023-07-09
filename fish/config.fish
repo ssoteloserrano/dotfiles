f3c0c85ee21d11b3a8cde3373161adeb821306e2
scarktt <ssoteloserrano@gmail.com>
2023-07-08 23:17:04 -0600

    feat: add lazygit alias

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ end
 
 oh-my-posh init fish --config ~/.config/.poshthemes/zash.modified.json | source
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/scarlett/google-cloud-sdk/path.fish.inc' ]; . '/home/scarlett/google-cloud-sdk/path.fish.inc'; end
-
 set -x PATH $PATH '/home/scarlett/.local/bin'
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/scar/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/home/scar/Downloads/google-cloud-sdk/path.fish.inc'; end
