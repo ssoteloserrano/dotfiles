2fa2afc527affe081fd54771e36f5f2521ecee34
scarktt <ssoteloserrano@gmail.com>
2023-07-08 23:18:27 -0600

    feat: add function to clear uname tmux sessions

---
+++ tmux/kill_uname_session.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+function tmux_kill_uname_session() {
+  echo "kill all unname tmux session"
+  cd /tmp/
+  tmux ls | awk '{print $1}' | grep -o '[0-9]\+' >/tmp/killAllUnnameTmuxSessionOutput.sh
+  sed -i 's/^/tmux kill-session -t /' killAllUnnameTmuxSessionOutput.sh
+  chmod +x killAllUnnameTmuxSessionOutput.sh
+  ./killAllUnnameTmuxSessionOutput.sh
+  cd -
+  tmux ls
+}
+
+tmux_kill_uname_session
