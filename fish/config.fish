0ee149bf46fc293e9ad69461fff210798b965185
scarktt <ssoteloserrano@gmail.com>
2023-07-09 18:11:37 -0600

    feat(fish): add plugin manager, exa and z plugin for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+  if type -q exa
+    alias ll "exa -l -g --icons"
+    alias lla "ll -a"
+  end
 end
 
 oh-my-posh init fish --config ~/.config/.poshthemes/zash.modified.json | source
