ce96e7304e9b55f137dd8badcafc0857e14d4d4f
scarktt <ssoteloserrano@gmail.com>
2023-07-16 17:33:28 -0600

    chore(fish): update GC PATH

--- fish/config.fish
+++ fish/config.fish
@@ -10,8 +10,8 @@ oh-my-posh init fish --config ~/.config/.poshthemes/zash.modified.json | source
 
 set -x PATH $PATH '/home/scarlett/.local/bin'
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/scar/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/home/scar/Downloads/google-cloud-sdk/path.fish.inc'; end
-
 # set -x PATH $PATH $GOPATH/bin
 set -Ua fish_user_paths '/home/scar/go/bin'
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/scar/google-cloud-sdk/path.fish.inc' ]; . '/home/scar/google-cloud-sdk/path.fish.inc'; end
