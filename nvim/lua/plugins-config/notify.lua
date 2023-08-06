f6970eec65906c202c86fe148f5d7fd67040b679
scarktt <ssoteloserrano@gmail.com>
2023-08-06 17:39:38 -0600

    feat(notify): add notification setup

--- nvim/lua/plugins-config/notify.lua
+++ nvim/lua/plugins-config/notify.lua
@@ -7,5 +7,19 @@ vim.notify = notify
 
 notify.setup {
   background_colour = "#000000",
+  fps = 60,
+  icons = {
+    DEBUG = "",
+    ERROR = "",
+    INFO = "",
+    TRACE = "✎",
+    WARN = ""
+  },
+  level = 2,
+  minimum_width = 50,
+  render = "default",
+  stages = "fade_in_slide_out",
+  timeout = 50,
+  top_down = true
 }
 
