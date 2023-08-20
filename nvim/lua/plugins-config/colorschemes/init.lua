69ba5b1a26ba222c1a2ed4b9127dab38a988fbf8
scarktt <ssoteloserrano@gmail.com>
2023-08-20 00:39:31 -0600

    style(nvim): add new colorscheme

--- nvim/lua/plugins-config/colorschemes/init.lua
+++ nvim/lua/plugins-config/colorschemes/init.lua
@@ -1,3 +1,4 @@
 
-require("plugins-config.colorschemes.catppuccin")
--- require("plugins-config.colorschemes.onedark")
+require("plugins-config.colorschemes.gruvbox-flat")
+-- require("plugins-config.colorschemes.catppuccin")
+-- require("plugins-config.colorschemes.tokyodark")
