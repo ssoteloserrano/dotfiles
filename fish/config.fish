2560c1f74acb0b6874f012aadea9fa0610bd0d88
scarktt <ssoteloserrano@gmail.com>
2023-11-18 14:09:11 -0600

    fish: remove env vars from config file

--- fish/config.fish
+++ fish/config.fish
@@ -8,10 +8,4 @@ end
 
 oh-my-posh init fish --config ~/.config/.poshthemes/zash.modified.json | source
 
-set -x PATH $PATH '/home/scarlett/.local/bin'
-
-# set -x PATH $PATH $GOPATH/bin
-set -Ua fish_user_paths '/home/scar/go/bin'
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/scar/google-cloud-sdk/path.fish.inc' ]; . '/home/scar/google-cloud-sdk/path.fish.inc'; end
+source ~/.config/fish/env_vars.fish
