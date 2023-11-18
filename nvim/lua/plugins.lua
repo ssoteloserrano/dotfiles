78098264cfb0c469cf6b390a0132b2e65e5efbe8
scarktt <ssoteloserrano@gmail.com>
2023-11-18 17:17:45 -0600

    lazy: basic migration

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,156 +1,126 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
     "git",
     "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
   })
-  print("Installing packer close and reopen Neovim...")
-  vim.cmd([[packadd packer.nvim]])
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
--- Use a protected call so we don"t error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
-
--- Have packer use a popup window
-packer.init({
-  display = {
-    open_fn = function()
-      return require("packer.util").float({ border = "rounded" })
-    end,
-  },
-})
-
-return packer.startup(function(use)
-  use("wbthomason/packer.nvim")
-
-  -- To improve startup time
-  use "lewis6991/impatient.nvim"
-
-  -- For neovim plugins dev
-  use { "nvim-lua/plenary.nvim" }
-
+local plugins = {
+  "nvim-lua/plenary.nvim", -- For neovim plugins dev
+  --
   -- Colorschemes
-  use "sainnhe/gruvbox-material"
-  use "Shatur/neovim-ayu"
+  "sainnhe/gruvbox-material",
+  "Shatur/neovim-ayu",
   -- with transparent options
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use "sainnhe/everforest"
-  use { "EdenEast/nightfox.nvim", tag = "v1.0.0" }
-  use "tiagovla/tokyodark.nvim"
-  use "navarasu/onedark.nvim"
-  use "eddyekofo94/gruvbox-flat.nvim"
-  use "folke/tokyonight.nvim"
-  use "rebelot/kanagawa.nvim"
+  { "catppuccin/nvim", name = "catppuccin" },
+  "sainnhe/everforest",
+  { "EdenEast/nightfox.nvim", version = "v1.0.0" },
+  "tiagovla/tokyodark.nvim",
+  "navarasu/onedark.nvim",
+  "eddyekofo94/gruvbox-flat.nvim",
+  "folke/tokyonight.nvim",
+  "rebelot/kanagawa.nvim",
 
-  use {
+  {
       "nvim-neorg/neorg",
       after = "nvim-treesitter",
       config = function()
         require("plugins-config.norg")
       end,
-      run = ":Neorg sync-parsers",
-      requires = "nvim-lua/plenary.nvim",
-  }
+      build = ":Neorg sync-parsers",
+      dependencies = "nvim-lua/plenary.nvim",
+  },
 
   -- Language highlighting
-  use {
+  {
     "nvim-treesitter/nvim-treesitter",
-    run = function()
+    build = function()
       local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
       ts_update()
     end,
-  }
+  },
 
   -- Icons
-  use "kyazdani42/nvim-web-devicons"
+  {
+    "kyazdani42/nvim-web-devicons",
+    dependencies = {
+      -- Buffers navigation and tabline
+      { "akinsho/bufferline.nvim", version = "v3.*" }
+    }
+  },
 
-  use "tzachar/local-highlight.nvim"
-
-  -- Buffers navigation and tabline
-  use { "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons" }
+  "tzachar/local-highlight.nvim",
 
   -- Lualine for statusline
-  use "nvim-lualine/lualine.nvim"
+  "nvim-lualine/lualine.nvim",
 
   -- Indentation guides
-  use "lukas-reineke/indent-blankline.nvim"
+  "lukas-reineke/indent-blankline.nvim",
 
   -- File search and much more
-  use { "junegunn/fzf", run = "fzf#install()" }
+  "junegunn/fzf", build = "fzf#install()",
 
   -- File explorer within nvim
-  use "kyazdani42/nvim-tree.lua"
+  "kyazdani42/nvim-tree.lua",
 
   -- Commenter
-  use "terrortylor/nvim-comment"
+  "terrortylor/nvim-comment",
 
   -- LSP Configuration
-  use {
+  {
     "neovim/nvim-lspconfig",
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
-  }
+  },
 
   -- Completion
-  use {
+  {
     "hrsh7th/nvim-cmp",
-    requires = {
+    dependencies = {
       {
         "L3MON4D3/LuaSnip",
-        requires = { "rafamadriz/friendly-snippets" },
+        dependencies = { "rafamadriz/friendly-snippets" },
         config = function()
           require("luasnip.loaders.from_vscode").lazy_load()
         end,
       },
-      { "hrsh7th/cmp-buffer",                  after = "nvim-cmp" },
-      { "hrsh7th/cmp-path",                    after = "nvim-cmp" },
-      { "hrsh7th/cmp-nvim-lua",                after = "nvim-cmp" },
-      { "hrsh7th/cmp-nvim-lsp",                after = "nvim-cmp" },
-      { "saadparwaiz1/cmp_luasnip",            after = "nvim-cmp" },
-      { "hrsh7th/cmp-nvim-lsp-signature-help", after = "nvim-cmp" },
+      { "hrsh7th/cmp-buffer",                  },
+      { "hrsh7th/cmp-path",                    },
+      { "hrsh7th/cmp-nvim-lua",                },
+      { "hrsh7th/cmp-nvim-lsp",                },
+      { "saadparwaiz1/cmp_luasnip",            },
+      { "hrsh7th/cmp-nvim-lsp-signature-help", },
     },
-  }
+  },
 
-  use "hrsh7th/cmp-nvim-lsp"
+  "hrsh7th/cmp-nvim-lsp",
 
   -- Insert parents, quotes and brackets in pair
-  use {
+  {
     "windwp/nvim-autopairs",
     config = function()
       require("plugins-config.autopairs")
     end,
-  }
+  },
 
   -- git
-  use {
+  {
     "lewis6991/gitsigns.nvim",
-    requires = { "nvim-lua/plenary.nvim" },
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("plugins-config.gitsigns")
     end,
-  }
+  },
 
-  use {
+  {
     "nvim-telescope/telescope.nvim",
-    requires = {
+    dependencies = {
       "nvim-lua/popup.nvim",
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope-file-browser.nvim",
@@ -160,71 +130,79 @@ return packer.startup(function(use)
     config = function()
       require("plugins-config.telescope")
     end,
-  }
+  },
 
-  use {
+  {
     "nvim-telescope/telescope-fzf-native.nvim",
-    run = "make",
-  }
+    build = "make",
+  },
 
-  use {
+  {
     "paopaol/telescope-git-diffs.nvim",
-    requires = {
+    dependencies = {
       "nvim-lua/plenary.nvim",
       "sindrets/diffview.nvim",
     },
-  }
+  },
 
   -- Pretty notifications
-  use "rcarriga/nvim-notify"
+  "rcarriga/nvim-notify",
 
-  use "kdheepak/lazygit.nvim"
+  "norcalli/nvim-colorizer.lua",
 
-  use "norcalli/nvim-colorizer.lua"
-
-  use {
+  {
     "iamcco/markdown-preview.nvim",
-    run = function() vim.fn["mkdp#util#install"]() end,
+    build = function() vim.fn["mkdp#util#install"]() end,
     config = function()
       require("plugins-config.markdown")
     end,
-  }
+  },
 
-  use "mg979/vim-visual-multi"
+  "mg979/vim-visual-multi",
 
   -- For laravel
-  use "jwalton512/vim-blade"
+  "jwalton512/vim-blade",
 
-  use { "akinsho/toggleterm.nvim", tag = "*" }
+  { "akinsho/toggleterm.nvim", version = "*" },
 
-  use "gelguy/wilder.nvim"
+  "gelguy/wilder.nvim",
 
   -- used for winbar
-  use {
+  {
     "SmiteshP/nvim-navic",
-    requires = "neovim/nvim-lspconfig"
-  }
+    dependencies = "neovim/nvim-lspconfig"
+  },
 
-  use "f-person/git-blame.nvim"
+  "f-person/git-blame.nvim",
 
-  use { "anuvyklack/windows.nvim",
-    requires = {
+  { "anuvyklack/windows.nvim",
+    dependencies = {
       "anuvyklack/middleclass",
       "anuvyklack/animation.nvim"
     },
     config = function()
       require("plugins-config.windows")
     end
-  }
+  },
 
-  use "lervag/vimtex"
+  "lervag/vimtex",
 
-  use {
+  {
     "m4xshen/hardtime.nvim",
-    requires = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" }
-  }
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" }
+  },
 
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+      }
+  }
+}
+
+require("lazy").setup(plugins)
