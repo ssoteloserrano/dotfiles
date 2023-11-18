355633706accf74e235229da2780b632e0389e8b
scarktt <ssoteloserrano@gmail.com>
2023-11-18 13:58:10 -0600

    polybar: comment temporarily updates counter

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -11,7 +11,7 @@ start_updates_counter() {
 
   pacman_updates=$(checkupdates | wc -l)
   aur_updates=$(yay -Qua | wc -l)
-  flatpak_updates=$(flatpak update -p | wc -l)
+  flatpak_updates=$(flatpak update | wc -l)
   total_updates=$((pacman_updates + aur_updates + flatpak_updates))
 
   export TOTAL_PKGS="%{T3}  %{T-}$total_updates"
@@ -43,7 +43,7 @@ start_bar() {
   echo "Bar launched..."
 }
 
-start_updates_counter
+# start_updates_counter &
 arg1="$1"
 bar=$(get_bar "$arg1")
 start_bar "$bar"
