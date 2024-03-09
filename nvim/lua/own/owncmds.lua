92a972284e6317f9611f1be53b5001072b54d822
scarktt <ssoteloserrano@gmail.com>
2024-03-08 21:50:11 -0600

    add indentation to printer

--- nvim/lua/own/owncmds.lua
+++ nvim/lua/own/owncmds.lua
@@ -1,9 +1,23 @@
+function GetCurrentLineIndentation()
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  -- Get the current cursor position
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local current_line = cursor[1]  -- Line number
+
+  -- Get the contents of the current line
+  local lines = vim.api.nvim_buf_get_lines(bufnr, current_line - 1, current_line, false)
+
+  -- Extract the indentation from the current line
+  local indentation = string.match(lines[1], "^%s*")
+  return indentation
+end
+
 function VariablePrinter()
   local variable = vim.fn.expand("<cword>")
   local log_line
 
   -- TODO: set print option format
-  -- TODO: get current indentation level
   -- TODO: print objects
   if vim.bo.filetype == "javascript" then
     log_line = "console.log('" .. variable .. " => ', JSON.stringify(" .. variable .. "));"
@@ -18,7 +32,10 @@ function VariablePrinter()
     return
   end
 
-  vim.fn.setreg("+", log_line)
+  local indentation = GetCurrentLineIndentation()
+  local msg_with_scope_indent = indentation .. log_line
+
+  vim.fn.setreg("+", msg_with_scope_indent)
 end
 
 local options = { noremap = true, silent = true }
