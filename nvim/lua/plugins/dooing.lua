8cb78d606d824cc612f90247dff5435e4f405e76
scarktt <ssoteloserrano@gmail.com>
2024-12-28 19:17:59 -0600

    feat: add dooing plugin

--- nvim/lua/plugins/dooing.lua
+++ nvim/lua/plugins/dooing.lua
@@ -1,18 +1,36 @@
 return {
-    "atiladefreitas/dooing",
-    config = function()
-        require("dooing").setup({
-          window = {
-              width = 55,
-              height = 20,
-              border = 'rounded',
-              padding = {
-                  top = 1,
-                  bottom = 5,
-                  left = 2,
-                  right = 2,
-              },
-          },
-        })
-    end,
+  "atiladefreitas/dooing",
+  config = function()
+    local icons = require("ui.icons")
+    local colors = require("ui.colors")
+
+    require("dooing").setup({
+      notes = {
+        icon = icons.File,
+      },
+      priority_groups = {
+        high = {
+          members = { "important", "urgent" },
+          color = colors.cyan,
+          hl_group = "DiagnosticError",
+        },
+        medium = {
+          members = { "important" },
+          color = colors.violet,
+          hl_group = "DiagnosticWarn",
+        },
+        low = {
+          members = { "urgent" },
+          color = colors.red,
+          hl_group = "DiagnosticInfo",
+        },
+      },
+      keymaps = {
+        open_todo_scratchpad = "<CR>",
+      },
+      scratchpad = {
+        syntax_highlight = "markdown"
+      }
+    })
+  end,
 }
