ca67fdb4a83740cc4df5b1d08c24c43a37f57d70
scarktt <ssoteloserrano@gmail.com>
2025-07-11 20:01:09 -0600

    lazy: update plugins

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -39,53 +39,53 @@ return {
           "cucumber_language_server",
         },
       })
-      mason_lspconfig.setup_handlers({
-        function(server_name)
-          -- We are overriding the default Emmet server setup handler
-          -- to have access to all html tags inside .ts and .js files
-          if server_name == "emmet_language_server" then
-            lsp_config["emmet_language_server"].setup({
-              filetypes = { "html", "typescript", "javascript" },
-              root_dir = lsp_config.util.find_git_ancestor,
-              on_attach = require("plugins.lsp.lsp-handlers").on_attach,
-              capabilities = require("plugins.lsp.lsp-handlers").capabilities,
-            })
-          elseif server_name == "pylsp" then
-            lsp_config["pylsp"].setup({
-              on_attach = require("plugins.lsp.lsp-handlers").on_attach,
-              capabilities = require("plugins.lsp.lsp-handlers").capabilities,
-              settings = {
-                pylsp = {
-                  plugins = {
-                    -- formatter options
-                    black = { enabled = true },
-                    autopep8 = { enabled = false },
-                    yapf = { enabled = false },
-                    -- linter options
-                    -- pylint = { enabled = true, executable = "pylint" },
-                    pyflakes = { enabled = true },
-                    -- pycodestyle = { enabled = true },
-                    -- type checker
-                    pylsp_mypy = { enabled = true },
-                    -- auto-completion options
-                    jedi_completion = { fuzzy = true },
-                    -- import sorting
-                    pyls_isort = { enabled = true },
-                  },
-                },
-              },
-              flags = {
-                debounce_text_changes = 200,
-              },
-            })
-          else
-            lsp_config[server_name].setup({
-              on_attach = require("plugins.lsp.lsp-handlers").on_attach,
-              capabilities = require("plugins.lsp.lsp-handlers").capabilities,
-            })
-          end
-        end,
-      })
+      -- mason_lspconfig.setup_handlers({
+      --   function(server_name)
+      --     -- We are overriding the default Emmet server setup handler
+      --     -- to have access to all html tags inside .ts and .js files
+      --     if server_name == "emmet_language_server" then
+      --       lsp_config["emmet_language_server"].setup({
+      --         filetypes = { "html", "typescript", "javascript" },
+      --         root_dir = lsp_config.util.find_git_ancestor,
+      --         on_attach = require("plugins.lsp.lsp-handlers").on_attach,
+      --         capabilities = require("plugins.lsp.lsp-handlers").capabilities,
+      --       })
+      --     elseif server_name == "pylsp" then
+      --       lsp_config["pylsp"].setup({
+      --         on_attach = require("plugins.lsp.lsp-handlers").on_attach,
+      --         capabilities = require("plugins.lsp.lsp-handlers").capabilities,
+      --         settings = {
+      --           pylsp = {
+      --             plugins = {
+      --               -- formatter options
+      --               black = { enabled = true },
+      --               autopep8 = { enabled = false },
+      --               yapf = { enabled = false },
+      --               -- linter options
+      --               -- pylint = { enabled = true, executable = "pylint" },
+      --               pyflakes = { enabled = true },
+      --               -- pycodestyle = { enabled = true },
+      --               -- type checker
+      --               pylsp_mypy = { enabled = true },
+      --               -- auto-completion options
+      --               jedi_completion = { fuzzy = true },
+      --               -- import sorting
+      --               pyls_isort = { enabled = true },
+      --             },
+      --           },
+      --         },
+      --         flags = {
+      --           debounce_text_changes = 200,
+      --         },
+      --       })
+      --     else
+      --       lsp_config[server_name].setup({
+      --         on_attach = require("plugins.lsp.lsp-handlers").on_attach,
+      --         capabilities = require("plugins.lsp.lsp-handlers").capabilities,
+      --       })
+      --     end
+      --   end,
+      -- })
     end,
   },
   {
