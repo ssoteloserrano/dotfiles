92f88fad6b4ce5018f5f42920037e0b997b675e3
scarktt <ssoteloserrano@gmail.com>
2025-07-11 20:00:51 -0600

    chore: comment unused functions

--- nvim/lua/plugins/nvim-treesitter-textobjects.lua
+++ nvim/lua/plugins/nvim-treesitter-textobjects.lua
@@ -1,5 +1,8 @@
+-- Este plugin lo tengo debido a que no he conseguido hacer funcionar algnas
+-- a/i por ejemplo el de functions o class con mini.ai
 return {
   "nvim-treesitter/nvim-treesitter-textobjects",
+  enabled = true,
   after = "nvim-treesitter",
   requires = "nvim-treesitter/nvim-treesitter",
   config = function()
@@ -16,9 +19,6 @@ return {
             ["ac"] = { query = "@class.outer", desc = "outer class" },
             ["ic"] = { query = "@class.inner", desc = "inner class" },
 
-            ["aa"] = { query = "@assignment.outer", desc = "outer assignment" },
-            ["ia"] = { query = "@assignment.inner", desc = "inner assignment" },
-
             -- In python this is return
             ["al"] = { query = "@call.outer", desc = "outer call" },
             ["il"] = { query = "@call.inner", desc = "inner call" },
@@ -29,15 +29,22 @@ return {
             ["ak"] = { query = "@block.outer", desc = "outer block" },
             ["ik"] = { query = "@block.inner", desc = "inner block" },
 
-
             ["ar"] = { query = "@parameter.outer", desc = "outer parameter" },
             ["ir"] = { query = "@parameter.inner", desc = "inner parameter" },
 
             ["ad"] = { query = "@conditional.outer", desc = "outer conditional" },
             ["id"] = { query = "@conditional.inner", desc = "inner conditional" },
 
-            ["aq"] = { query = "@comment.outer", desc = "outer comment" },
-            ["iq"] = { query = "@comment.inner", desc = "inner comment" },
+            -- Comenté esto debido a que me dí cuenta que interfería con el `argument`
+            -- de mini.ai
+            -- ["aa"] = { query = "@assignment.outer", desc = "outer assignment" },
+            -- ["ia"] = { query = "@assignment.inner", desc = "inner assignment" },
+
+
+            -- Comenté esto debido a que me dí cuenta que interfería con el `quote`
+            -- de mini.ai
+            -- ["aq"] = { query = "@comment.outer", desc = "outer comment" },
+            -- ["iq"] = { query = "@comment.inner", desc = "inner comment" },
           },
         },
       }
