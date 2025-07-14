39c8df378c5b7a741377736a1a0193ef52feb82c
scarktt <ssoteloserrano@gmail.com>
2025-07-14 15:54:55 -0600

    feat: add new providers

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -5,46 +5,108 @@ return {
     event = "VeryLazy",
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
+    build = "make",
     opts = {
-      provider = "gemini",
+      provider = "llama_3_3_nemotron",
+      providers = {
+        gemini = {
+          model = "gemini-2.5-flash-preview-04-17",
+        },
+        deepseek_r1_0528 = {
+          -- LLM base: DeepSeek Coder
+          __inherited_from = "openai",
+          api_key_name = "OPENROUTER_API_KEY",
+          endpoint = "https://openrouter.ai/api/v1",
+          model = "deepseek/deepseek-r1-0528:free",
+          extra_request_body = {
+            temperature = 0.7,
+            max_tokens = 1000,
+            reasoning_effort = "low",
+          },
+          disable_tools = true,
+        },
+        deepseek_r1_0528_qwen3 = {
+          -- LLM base: Qwen3-8B (generalista)
+          __inherited_from = "openai",
+          api_key_name = "OPENROUTER_API_KEY",
+          endpoint = "https://openrouter.ai/api/v1",
+          model = "deepseek/deepseek-r1-0528-qwen3-8b:free",
+          extra_request_body = {
+            temperature = 0.7,
+            max_tokens = 1000,
+            reasoning_effort = "low",
+          },
+          disable_tools = true,
+        },
+        llama_3_3_nemotron = {
+          -- LLM base: Meta's Llama-3.3
+          __inherited_from = "openai",
+          api_key_name = "OPENROUTER_API_KEY",
+          endpoint = "https://openrouter.ai/api/v1",
+          model = "nvidia/llama-3.3-nemotron-super-49b-v1:free",
+          extra_request_body = {
+            temperature = 0.7,
+            reasoning_effort = "low",
+          },
+          disable_tools = true,
+        },
+        openrouter_devstral_small_2505 = {
+          __inherited_from = "openai",
+          api_key_name = "OPENROUTER_API_KEY",
+          endpoint = "https://openrouter.ai/api/v1",
+          model = "mistralai/devstral-small-2505:free",
+          extra_request_body = {
+            temperature = 0.7,
+            reasoning_effort = "low",
+          },
+          disable_tools = true,
+        },
+        openrouter_mistral_small_3_2 = {
+          __inherited_from = "openai",
+          api_key_name = "OPENROUTER_API_KEY",
+          endpoint = "https://openrouter.ai/api/v1",
+          model = "mistralai/mistral-small-3.2-24b-instruct:free",
+          disable_tools = true,
+        },
+      },
       behaviour = {
         enable_cursor_planning_mode = true,
+        support_paste_from_clipboard = true,
+        auto_suggestions = false,
       },
-      gemini = {
-        model = "gemini-2.5-flash-preview-04-17",
+      mappings = {
+        ask = "<leader>aa",
+        edit = "<leader>ae",
+        refresh = "<leader>ar",
+        clear = "<leader>ac",
+        stop = "<leader>as",
       },
     },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",      -- for providers='copilot'
+      -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "echasnovski/mini.icons", -- or echasnovski/mini.icons
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
         event = "VeryLazy",
         opts = {
-          -- recommended settings
           default = {
             embed_image_as_base64 = false,
             prompt_for_file_name = false,
             drag_and_drop = {
               insert_mode = true,
             },
-            -- required for Windows users
             use_absolute_path = true,
           },
         },
       },
       {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
+        "MeanderingProgrammer/render-markdown.nvim",
         opts = {
           file_types = { "markdown", "Avante" },
         },
